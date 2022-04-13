6a19d47d25ed97af7d3a011d29c4d8bf492a1549
Serhii Karavashkin <ksawl@narod.ru>
2022-04-13 22:49:05 +0300

    add flexible layout

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,13 +1,35 @@
 import "./boardComponent.scss";
-import React, { Dispatch } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { CellAction, CellState } from "../type";
+import React, { useCallback, useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { CellState } from "../type";
 import CellComponent from "./CellComponent";
 import { labelChars, labelNums } from "../store/initState";
 
 const BoardComponent = () => {
-  const dispatch: Dispatch<CellAction> = useDispatch();
   const cells = useSelector((state: CellState) => state.cells);
+  let [boardSide, setBoardSide] = useState(
+    Math.round(
+      (window.innerWidth > window.innerHeight
+        ? window.innerHeight
+        : window.innerWidth) * 0.9
+    )
+  );
+
+  let resizeWindow = useCallback(() => {
+    setBoardSide(
+      Math.round(
+        (window.innerWidth > window.innerHeight
+          ? window.innerHeight
+          : window.innerWidth) * 0.9
+      )
+    );
+  }, []);
+
+  useEffect(() => {
+    resizeWindow();
+    window.addEventListener("resize", resizeWindow);
+    return () => window.removeEventListener("resize", resizeWindow);
+  }, [resizeWindow]);
 
   const horLabels = (
     <div className="hor-label">
@@ -27,7 +49,10 @@ const BoardComponent = () => {
 
   // console.log(state);
   return (
-    <div className="board">
+    <div
+      className="board"
+      style={{ width: `${boardSide}px`, height: `${boardSide}px` }}
+    >
       <div></div>
       {horLabels}
       <div></div>
